735d925ab4cdd2379f48ee3e80fcab68344f9147
ylahti6 <ylahti6@gmail.com>
2024-05-21 13:49:24 +0100

    working on app structure

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import Navigator from "./components/Navigator";
 import HomePage from "./pages/HomePage";
 import Footer from "./components/Footer";
 
+import "./styles/App.css";
+
 function App() {
   return (
     <div className="App">
       <Navigator />
       <HomePage />
-      <Footer />
+      <Footer className="no-wrap"/>
     </div>
   );
 }
